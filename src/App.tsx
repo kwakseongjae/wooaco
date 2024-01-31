d63154226dc7f063c8f787baa3078b34cd8d2c01
kwakseongjae <gkffhdnls13@gmail.com>
2024-01-31 10:07:20 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="wrapper">
+          <div className="header_logo">
+            <a href="#">우아코</a>
+          </div>
+          <div className="header_nav">
+            <div>카테고리</div>
+            <div>로그인</div>
+            <div>회원가입</div>
+          </div>
+        </div>
       </header>
     </div>
   );
