d6b98ea0f9cc4b6497895ff6a1c2087c6f2f6e85
kwakseongjae <gkffhdnls13@gmail.com>
2024-02-07 09:06:02 +0900

    remove: CodeboxGrid.tsx 삭제 - Terminal과 CodeBox 컴포넌트의 위계를 맞춤

--- src/components/Codebox.tsx
+++ src/components/Codebox.tsx
@@ -1,9 +1,103 @@
-import CodeboxGrid from "./CodeboxGrid";
+import { useState } from "react";
+import useBearStore from "@store/store";
+import next from "@assets/next.png";
+import edit from "@assets/edit.png";
+import arrowDown from "@assets/arrowdown.png";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+
+const CODE_CONTENT = "CODE_CONTENT";
+
+const selectList = [
+  { value: "python", name: "Python" },
+  { value: "javascript", name: "Javascript" },
+  { value: "html", name: "HTML" },
+  { value: "bash", name: "Bash" },
+];
 
 function Codebox() {
+  const [content, setContent] = useState<string>(" ");
+  const [selected, setSelected] = useState("python");
+  const firstButtonValue = useBearStore((state) => state.firstButtonValue);
+  const increaseFirstButtonValue = useBearStore(
+    (state) => state.increaseFirstButton
+  );
+  const decreaseFirstButtonValue = useBearStore(
+    (state) => state.decreaseFirstButton
+  );
+
+  function onChange(e: any) {
+    setContent(e.target.value);
+  }
+
+  const handleSelect = (e: any) => {
+    setSelected(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (content) {
+      localStorage.setItem(CODE_CONTENT, content);
+      increaseFirstButtonValue();
+    }
+  };
   return (
     <div className="codebox">
-      <CodeboxGrid />
+      <div className="lang-select">
+        <p>언어는</p>
+        <div className="selectBox">
+          <select
+            name="PL"
+            className="select"
+            onChange={handleSelect}
+            value={selected}
+          >
+            {selectList.map((item) => {
+              return (
+                <option value={item.value} key={item.value}>
+                  {item.name}
+                </option>
+              );
+            })}
+          </select>
+          <span className="icoArrow">
+            <img src={arrowDown} alt="arrow" />
+          </span>
+        </div>
+      </div>
+      <div className="codebox-container">
+        {firstButtonValue ? null : (
+          <div className="codebox-typing">
+            <textarea onChange={onChange} />
+          </div>
+        )}
+
+        <div
+          className="codebox-result"
+          style={firstButtonValue ? { width: "100%" } : {}}
+        >
+          {firstButtonValue ? (
+            <div className="edit-box">
+              <div></div>
+              <button
+                className="edit"
+                onClick={() => decreaseFirstButtonValue()}
+              >
+                <p>수정하기</p>
+                <img src={edit} alt="edit" />
+              </button>
+            </div>
+          ) : null}
+          <SyntaxHighlighter language={selected}>{content}</SyntaxHighlighter>
+        </div>
+      </div>
+      {firstButtonValue ? (
+        <></>
+      ) : (
+        <div className="next-btn">
+          <button className="icon-button" onClick={handleSubmit}>
+            <img src={next} alt="next" /> 다음
+          </button>
+        </div>
+      )}
     </div>
   );
 }
